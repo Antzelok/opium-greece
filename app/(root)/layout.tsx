acf12a6e069c512def9d181c28a00b05c9fa43a4
Antzelo <akermezo@yahoo.com>
2026-04-29 09:24:51 +0300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "../components/layout/footer";
+import Header from "../components/layout/header";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    template: `%s | Prostore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
