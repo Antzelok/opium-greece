33bd6645216250ee6213145056c8518f3675dd09
Antzelo <akermezo@yahoo.com>
2026-05-01 09:52:41 +0300

    fix middleware function

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,13 @@
 import NextAuth from "next-auth";
 import { AuthConfig } from "./auth.config";
 
-export const { auth: middleware } = NextAuth(AuthConfig);
+// We initialize NextAuth with our config
+const { auth } = NextAuth(AuthConfig);
+
+// We export the auth function as the middleware
+export default auth;
+
+export const config = {
+  // Matcher to skip all internal paths, api routes and static files
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
