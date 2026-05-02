eb2d06cbb5d3a1419877deff01e556698e226b47
Antzelo <akermezo@yahoo.com>
2026-05-02 11:29:55 +0300

    get my cart in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,20 @@
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import { Toaster } from "sonner";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cart = await getMyCart();
   return (
     <div className="flex min-h-dvh flex-col bg-black pt-15">
-      <Header />
+      <Header cart={cart} />
       <main className="flex-1 wrapper">{children}</main>
       <Footer />
+      <Toaster position="top-left" />
     </div>
   );
 }
