c2217b9243e0bb84e064b3b67c26bb69d955e7f6
Antzelo <akermezo@yahoo.com>
2026-06-24 13:59:41 +0300

    update page error fix

--- app/(root)/admin/products/[id]/page.tsx
+++ app/(root)/admin/products/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getProductById } from "@/lib/actions/product.actions";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { requireAdmin } from "@/lib/auth-guard";
+import { Product } from "@/types";
 
 export const metadata: Metadata = {
   title: "Update Product",
@@ -25,7 +26,7 @@ const AdminProductUpdatePage = async (props: {
     <div className="space-y-8 max-w-5xl mx-auto">
       <h1 className="h2-bold">Update Product</h1>
 
-      <ProductForm type="Update" product={product} />
+      <ProductForm type="Update" product={product as Product} />
     </div>
   );
 };
