c1adebbbcfab5a22f43eaa43b71724ab4efae348
Antzelo <akermezo@yahoo.com>
2026-07-01 11:32:13 +0300

    stores page

---
+++ app/(root)/stores/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import StoreLocations from "@/components/home/store-locations";
+
+const StoresPage = () => {
+  return (
+    <main className="bg-black min-h-screen text-white">
+      <StoreLocations />
+    </main>
+  );
+};
+
+export default StoresPage;
